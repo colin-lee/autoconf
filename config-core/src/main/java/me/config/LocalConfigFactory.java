d4ac31cedd6fc8f81ba5dbe6df85fed9cccee9db
colin-lee <39513055@qq.com>
2015-10-01 12:50:38 +0800

    优化实现

--- config-core/src/main/java/me/config/LocalConfigFactory.java
+++ config-core/src/main/java/me/config/LocalConfigFactory.java
@@ -1,28 +1,22 @@
 package me.config;
 
-import com.google.common.base.CharMatcher;
-import com.google.common.base.Splitter;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-import me.config.api.IChangeListener;
 import me.config.api.IChangeableConfig;
-import me.config.api.IConfigFactory;
 import me.config.api.IFileListener;
+import me.config.base.AbstractConfigFactory;
 import me.config.impl.LocalConfig;
-import me.config.impl.MergedConfig;
 import me.config.watcher.FileUpdateWatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.nio.file.Path;
-import java.util.List;
 import java.util.concurrent.ConcurrentMap;
 
 /**
  * 本地配置工厂
  * Created by lirui on 2015-09-30 22:25.
  */
-public class LocalConfigFactory implements IConfigFactory {
+public class LocalConfigFactory extends AbstractConfigFactory {
 	private final Logger log = LoggerFactory.getLogger(getClass());
 	private final ConcurrentMap<String, IChangeableConfig> m = Maps.newConcurrentMap();
 	private final Path basePath;
@@ -31,63 +25,15 @@ public class LocalConfigFactory implements IConfigFactory {
 		this.basePath = basePath;
 	}
 
-	@Override
-	public IChangeableConfig getConfig(String name) {
-		IChangeableConfig c = m.get(name);
-		if (c == null) {
-			synchronized (this) {
-				c = m.get(name);
-				if (c == null) {
-					c = newConfig(name);
-					IChangeableConfig real = m.putIfAbsent(name, c);
-					if (real != null) {
-						c = real;
-					}
-				}
-			}
-		}
-		return c;
-	}
-
-	@Override
-	public IChangeableConfig getConfig(String name, IChangeListener listener) {
-		return getConfig(name, listener, true);
-	}
-
-	@Override
-	public IChangeableConfig getConfig(String name, IChangeListener listener, boolean loadAfterRegister) {
-		IChangeableConfig config = getConfig(name);
-		config.addListener(listener, loadAfterRegister);
-		return config;
-	}
-
-	@Override
-	public boolean hasConfig(String name) {
-		return m.containsKey(name);
-	}
-
-	private IChangeableConfig newConfig(String name) {
-		CharMatcher matcher = CharMatcher.anyOf(",; |");
-		if (matcher.matchesAnyOf(name)) {
-			List<String> names = Splitter.on(matcher).trimResults().omitEmptyStrings().splitToList(name);
-			List<IChangeableConfig> list = Lists.newArrayList();
-			for (String i : names) {
-				list.add(getConfig(i));
-			}
-			return new MergedConfig(list);
-		} else {
-			return newConfig(name, basePath.resolve(name));
-		}
-	}
-
 	/**
 	 * 创建LocalConfig并增加更新回调功能
 	 *
 	 * @param name 配置名
-	 * @param path 本地路径
 	 * @return 配置
 	 */
-	private LocalConfig newConfig(String name, Path path) {
+	@Override
+	protected IChangeableConfig doCreate(String name) {
+		Path path = basePath.resolve(name);
 		final LocalConfig c = new LocalConfig(name, path);
 		FileUpdateWatcher.getInstance().watch(path, new IFileListener() {
 			@Override
