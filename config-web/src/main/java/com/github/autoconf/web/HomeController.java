beea0cb227d82b7cf95d550d945b8a96efe4426a
colin-lee <39513055@qq.com>
2015-10-04 14:08:20 +0800

    优化项目借款购

---
+++ config-web/src/main/java/com/github/autoconf/web/HomeController.java
@@ -0,0 +1,11 @@
+package com.github.autoconf.web;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 业务主程
+ * Created by lirui on 2015-10-04 13:35.
+ */
+@Controller
+public class HomeController {
+}
