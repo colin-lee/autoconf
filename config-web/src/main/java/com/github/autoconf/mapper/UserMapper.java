45bac5f8d092b11dd74bda1acde4c1fe9a82b40d
colin-lee <39513055@qq.com>
2015-10-04 23:28:48 +0800

    初始化config-web

--- config-web/src/main/java/com/github/autoconf/mapper/UserMapper.java
+++ config-web/src/main/java/com/github/autoconf/mapper/UserMapper.java
@@ -3,11 +3,13 @@ package com.github.autoconf.mapper;
 import com.github.autoconf.entity.User;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
 /**
  * 用户相关操作
  * Created by lirui on 2015-10-04 19:11.
  */
+@Repository
 public interface UserMapper {
   @Select("SELECT * FROM user WHERE username=#{username}")
   User findByUserName(@Param("username") String username);
